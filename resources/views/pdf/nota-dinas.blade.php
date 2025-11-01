c577fe418abca9b7b5527b4591d7e8509009d4d2
Robbys <robbys@Robbyss-MacBook-Pro.local>
2025-11-01 21:53:20 +0800

    perbarui judul halaman Nota Dinas untuk menyertakan nomor nota dinas

--- resources/views/pdf/nota-dinas.blade.php
+++ resources/views/pdf/nota-dinas.blade.php
@@ -7,7 +7,7 @@
         content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible"
         content="ie=edge">
-    <title>Nota Dinas</title>
+    <title>Nota Dinas - {{ $surat->nomor_nota_dinas }}</title>
     <style>
         * {
             font-family: Arial, sans-serif;
