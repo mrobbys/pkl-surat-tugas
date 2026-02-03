701136041b998243e41e315557629ad72bbe533f
mrobbys <mhmmdrobby48@gmail.com>
2026-02-03 20:56:47 +0800

    refactor: fix typo in ActivityController filename and update references

--- resources/js/activity-logs/index.js
+++ resources/js/activity-logs/index.js
@@ -1,7 +1,11 @@
-import { formatTanggalWaktuIndonesia } from '../utils/dateFormatterIndonesia.js'
-
 export const indexMethods = {
   fetchActivities() {
+    const self = this;
+
+    if(this.dataTable){
+      this.dataTable.destroy();
+    }
+
     this.dataTable = new DataTable("#activity-log-table", {
       layout: {
         bottomEnd: {
@@ -26,9 +30,9 @@ export const indexMethods = {
         name: 'causer.email',
         render: function (data, type, row) {
           if (data && data.email) {
-            return data.email
+            return self.sanitizeString(data.email)
           } else if (row.properties && row.properties.email) {
-            return `<span>${row.properties.email}</span>`
+            return `<span>${self.sanitizeString(row.properties.email)}</span>`
           } else {
             return `<span class="text-gray-400 italic">System</span>`
           }
@@ -36,29 +40,31 @@ export const indexMethods = {
       },
       {
         data: 'description',
-        name: 'description'
+        name: 'description',
+        render: (data) => self.sanitizeString(data)
       },
       {
         data: 'created_at',
         name: 'created_at',
-        render: function (data, type, row) {
-          return formatTanggalWaktuIndonesia(data);
-        }
+        render: (data) => self.formatTanggalWaktuIndonesia(data),
       },
       {
         data: 'properties',
         name: 'properties.ip_address',
         render: function (data, type, row) {
-          return data ? data.ip_address : `<span class="text-gray-400">-</span>`;
+          return data ? self.sanitizeString(data.ip_address) : `<span class="text-gray-400">-</span>`;
         }
       },
       {
         data: 'log_name',
-        name: 'log_name'
+        name: 'log_name',
+        render: (data) => self.sanitizeString(data)
       }
       ],
     });
 
     window.alpineComponent = this;
   },
+
+  
 }
--- resources/js/managers/activityManager.js
+++ resources/js/managers/activityManager.js
@@ -1,10 +1,37 @@
-import { indexMethods } from "../activity-logs"
+import { indexMethods } from "../activity-logs/index.js"
+import { sanitizeString } from '../utils/sanitizeString.js'
+import { formatTanggalWaktuIndonesia } from '../utils/dateFormatterIndonesia.js'
 
 function activityManager(config) {
   return {
     config: config,
+    dataTable: null,
 
     ...indexMethods,
+
+    // sanitize string
+    sanitizeString(str) {
+      return sanitizeString(str);
+    },
+
+    // format date to Indonesian format
+    formatTanggalWaktuIndonesia(dateString) {
+      return formatTanggalWaktuIndonesia(dateString);
+    },
+
+    // cleanup
+    destroy() {
+      this.destroyDataTable();
+    },
+
+    // destory datatable
+    destroyDataTable() {
+      if (this.dataTable) {
+        this.dataTable.destroy();
+        this.dataTable = null;
+      }
+    }
+
   }
 }
 
